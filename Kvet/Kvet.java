c89dcc5a110c898f782c56720bc8032b32cc5424
Dzuri <dzurakmatej@gmail.com>
2022-11-11 13:11:34 +0100

    vytvorenie kvetu

---
+++ Kvet/Kvet.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Kvet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Kvet {
+    /**
+     * Constructor for objects of class Kvet
+     */
+    public Kvet() {
+        // initialise instance variables
+    }
+}
