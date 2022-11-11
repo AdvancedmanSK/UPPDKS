9ffa37780543896cbead1b55cec9dd1f977718d2
AdvancedmanSK <patrik.sramek0@gmail.com>
2022-11-11 13:18:31 +0100

    Pridanie mravcov.java

---
+++ Kvet/Mravce.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Mravce here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mravce {
+    /**
+     * Constructor for objects of class Mravce
+     */
+    public Mravce() {
+        // initialise instance variables
+    }
+}
